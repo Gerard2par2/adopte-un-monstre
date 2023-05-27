6bfbe6dee44ebeec2c82698bbd6b3de901aa4663
Simard <floogles71@gmail.com>
2023-05-27 12:56:35 +0200

    initialized spring prooject

---
+++ adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/AdopteUnMonstreApplication.java
@@ -0,0 +1,13 @@
+package com.simard.adopteunmonstre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopteUnMonstreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopteUnMonstreApplication.class, args);
+	}
+
+}
---
+++ adopte-un-monstre/adopte-ton-monstre-api/src/test/java/com/simard/adopteunmonstre/AdopteUnMonstreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simard.adopteunmonstre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopteUnMonstreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
