e3fdb268e681fd7d45b4286f876c64364062f145
Simard <floogles71@gmail.com>
2023-05-27 13:50:56 +0200

    created method findByType

--- adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/repository/MonsterRepository.java
+++ adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/repository/MonsterRepository.java
@@ -1,9 +1,13 @@
 package com.simard.adopteunmonstre.repository;
 
 import com.simard.adopteunmonstre.model.entities.Monster;
+import com.simard.adopteunmonstre.model.entities.MonsterType;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MonsterRepository extends CrudRepository<Monster, Long> {
+    List<Monster> findByType(MonsterType type);
 }
