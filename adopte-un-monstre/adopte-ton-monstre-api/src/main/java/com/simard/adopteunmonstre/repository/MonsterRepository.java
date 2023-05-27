5595bec3ecac1e4c32792c4f3cecfbe714905ba0
Simard <floogles71@gmail.com>
2023-05-27 14:34:45 +0200

    fix broken queries

--- adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/repository/MonsterRepository.java
+++ adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/repository/MonsterRepository.java
@@ -1,7 +1,10 @@
 package com.simard.adopteunmonstre.repository;
 
 import com.simard.adopteunmonstre.model.entities.Monster;
+import com.simard.adopteunmonstre.model.entities.MonsterMonsterType;
 import com.simard.adopteunmonstre.model.entities.MonsterType;
+import jakarta.persistence.JoinColumn;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,10 @@ import java.util.List;
 
 @Repository
 public interface MonsterRepository extends CrudRepository<Monster, Long> {
-    List<Monster> findByType(MonsterType type);
+    @Query("SELECT m FROM Monster m where m.id = (select a.monster.id from Adoption a where a.user.id = :userId) ")
+    List <Monster> findAllByUserId(Long userId);
+
+    @Query("SELECT m FROM Monster m where m.id in (select mmt.monster.id from MonsterMonsterType mmt where mmt.monsterType in :monsterMonsterTypes)")
+    List<Monster> findByMonsterMonsterTypes(List<MonsterMonsterType> monsterMonsterTypes);
+
 }
