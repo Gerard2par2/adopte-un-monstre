08058d9e7afbbb7a9f7b234a0dd2fd2fac26e6f6
Simard <floogles71@gmail.com>
2023-05-27 13:57:46 +0200

    created method findAllByUserId

--- adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/repository/AdoptionRepository.java
+++ adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/repository/AdoptionRepository.java
@@ -1,9 +1,13 @@
 package com.simard.adopteunmonstre.repository;
 
 import com.simard.adopteunmonstre.model.entities.Adoption;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdoptionRepository extends CrudRepository<Adoption, Long> {
+
+    @Query("SELECT a FROM Adoption a WHERE a.user.id = :userId")
+    Iterable<Adoption> findAllByUserId(Long userId);
 }
