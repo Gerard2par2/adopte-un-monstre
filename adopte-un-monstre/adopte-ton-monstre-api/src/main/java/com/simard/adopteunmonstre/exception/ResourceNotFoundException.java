50f726bca625228528d0e6a1e582b0d543227e38
Simard <floogles71@gmail.com>
2023-05-27 14:11:15 +0200

    created exception RessourceNotFoundException

--- adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/exception/ResourceNotFoundException.java
+++ adopte-un-monstre/adopte-ton-monstre-api/src/main/java/com/simard/adopteunmonstre/exception/ResourceNotFoundException.java
@@ -1,3 +1,5 @@
+package com.simard.adopteunmonstre.exception;
+
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
